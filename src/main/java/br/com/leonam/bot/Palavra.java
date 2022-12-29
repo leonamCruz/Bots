506d8b55c7a0c50d4a541ebe27a37e06c6f4bd4d
leonam <leonam447@gmail.com>
2022-12-29 11:34:17 -0300

    Commit

--- src/main/java/br/com/leonam/bot/Palavra.java
+++ src/main/java/br/com/leonam/bot/Palavra.java
@@ -1,6 +1,12 @@
 package br.com.leonam.bot;
 
-public class VisualizadorDeStories {
-    public static final String USER = "";
-    public static final String PASSWORD = "";
+public class Palavra {
+    public static final String USER = "seujurandidointellij";
+    public static final String PASSWORD = "Leonam1!";
+    public static final String SITE_INSTAGRAM = "https://www.instagram.com/";
+    public static final String XPATH_USERNAME = "/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[1]/div/label/input";
+    public static final String XPATH_STORIE_UM = "/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/section/main/div[1]/section/div[1]/div[2]/div/div/div/div/ul/li[3]/div/button";
+    public static final String NAO_SALVAR = "/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/section/main/div/div/div/div/button";
+    public static final String AGORA_NAO = "/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]";
+    public static final String TELA_STORIES = "/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/section";
 }
--- src/main/java/br/com/leonam/bot/UtilLogin.java
+++ src/main/java/br/com/leonam/bot/UtilLogin.java
@@ -1,7 +1,62 @@
 package br.com.leonam.bot;
 
-public class VisualizadorDeStories {
-    public static void main(String[] args) {
-        
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.Random;
+
+public class UtilLogin {
+    private final WebDriver webDriver;
+    private Actions actions;
+    private final Random random;
+
+    private UtilLogin() {
+        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\driver\\chromedriver.exe");
+        webDriver = new ChromeDriver();
+        actions = new Actions(webDriver);
+        random = new Random();
     }
+
+    public void login() throws InterruptedException {
+        webDriver.get(Palavra.SITE_INSTAGRAM);
+        Thread.sleep(1500);
+        webDriver.findElement(By.xpath(Palavra.XPATH_USERNAME)).click();
+        actions.sendKeys(Palavra.USER);
+        actions.sendKeys(Keys.TAB);
+        actions.sendKeys(Palavra.PASSWORD);
+        actions.sendKeys(Keys.ENTER);
+        actions.build().perform();
+        Thread.sleep(3500);
+        webDriver.findElement(By.xpath(Palavra.NAO_SALVAR)).click();
+        Thread.sleep(1500);
+        webDriver.findElement(By.xpath(Palavra.AGORA_NAO)).click();
+    }
+
+    public void verTodosOsStories() throws InterruptedException {
+        webDriver.findElement(By.xpath(Palavra.XPATH_STORIE_UM)).click();
+        try {
+            while (true) {
+                actions = new Actions(webDriver);
+                actions.sendKeys(Keys.ARROW_RIGHT);
+                actions.build().perform();
+                Thread.sleep(random.nextInt(1001, 4568));
+                webDriver.findElement(By.xpath(Palavra.TELA_STORIES));
+            }
+        }catch (NoSuchElementException e){
+            System.out.println("Não achei");
+            System.out.println("Vou finalizar hehehe");
+            System.exit(0);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        var bot = new UtilLogin();
+        bot.login();
+        bot.verTodosOsStories();
+    }
+
 }
