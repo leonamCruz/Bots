16b76bb1b5d90616d99de26b998db382460384f9
leonam <leonam447@gmail.com>
2022-12-29 11:33:38 -0300

    Initial commit

---
+++ src/main/java/br/com/leonam/bot/Palavra.java
@@ -0,0 +1,6 @@
+package br.com.leonam.bot;
+
+public class VisualizadorDeStories {
+    public static final String USER = "";
+    public static final String PASSWORD = "";
+}
---
+++ src/main/java/br/com/leonam/bot/UtilLogin.java
@@ -0,0 +1,7 @@
+package br.com.leonam.bot;
+
+public class VisualizadorDeStories {
+    public static void main(String[] args) {
+        
+    }
+}
