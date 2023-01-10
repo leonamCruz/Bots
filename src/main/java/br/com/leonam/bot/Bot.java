f41192011bd7873b944908dd16d1e33dc7952a16
leonam <leonam447@gmail.com>
2023-01-10 13:13:29 -0300

    att View

--- src/main/java/br/com/leonam/bot/Bot.java
+++ src/main/java/br/com/leonam/bot/Bot.java
@@ -2,7 +2,6 @@ package br.com.leonam.bot;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
